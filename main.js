d2c6e09e20686b4a8eccba3ad83c314f21cbec33
kgangadhar <karthikg1643@gmail.com>
2020-06-07 03:49:17 -0500

    added unit test cases for stations endpoint

--- main.js
+++ main.js
@@ -8,22 +8,37 @@ const server = require('./lib/server');
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
 
-if (cluster.isMaster) {
-  console.log(`Master ${process.pid} is running`);
+// Declare the app
+const app = {};
 
-  // Fork workers.
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
+// Init function
+app.init = function () {
+
+  if (cluster.isMaster) {
+    console.log(`Master ${process.pid} is running`);
+
+    // Fork workers.
+    for (let i = 0; i < numCPUs; i++) {
+      cluster.fork();
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+      console.log(`worker ${worker.process.pid} died`);
+    });
+
+  } else {
+
+  // Start the server
+    server.init();
+    let message = `worker ${process.pid}...`;
+    console.log(message);
+
   }
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
-  });
+};
 
-} else {
+// Self executing
+app.init();
 
-  server.init();
-  let message = `worker ${process.pid}...`;
-  console.log(message);
-
-}
+// Export the app
+module.exports = app;
---
+++ test/stations.js
@@ -0,0 +1,91 @@
+/*
+ * API Tests
+ *
+ */
+
+// Dependencies
+var app = require('./../main');
+var assert = require('assert');
+var http = require('http');
+var config = require('./../lib/config');
+
+// Holder for Tests
+var api = {};
+
+// Helpers
+var helpers = {};
+helpers.makeGetRequest = function(path,callback){
+  // Configure the request details
+  var requestDetails = {
+    'protocol' : 'http:',
+    'hostname' : 'localhost',
+    'port' : config.httpPort,
+    'method' : 'GET',
+    'path' : path,
+    'headers' : {
+      'Content-Type' : 'application/json',
+      'authorization' : config.apikey
+    }
+  };
+
+  // Send the request
+  var req = http.request(requestDetails,function(res){
+      callback(res);
+  });
+  req.end();
+};
+
+helpers.makeGetRequestWithoutAPIKey = function(path,callback){
+    // Configure the request details
+    var requestDetails = {
+      'protocol' : 'http:',
+      'hostname' : 'localhost',
+      'port' : config.httpPort,
+      'method' : 'GET',
+      'path' : path,
+      'headers' : {
+        'Content-Type' : 'application/json',
+      }
+    };
+  
+    // Send the request
+    var req = http.request(requestDetails,function(res){
+        callback(res);
+    });
+    req.end();
+  };
+
+// Make a request to /ping
+api['/ping should respond to GET with 404'] = function(done){
+  helpers.makeGetRequest('/ping',function(res){
+    assert.equal(res.statusCode,404);
+    done();
+  });
+};
+
+// Make a request to /stations
+api['/stations?station_id=91 should respond to GET with 200'] = function(done){
+  helpers.makeGetRequest('/stations?station_id=91',function(res){
+    assert.equal(res.statusCode,200);
+    done();
+  });
+};
+
+// Make a request to /stations without api key
+api['/stations?station_id=91 should respond to GET with 400'] = function(done){
+    helpers.makeGetRequestWithoutAPIKey('/stations?station_id=91',function(res){
+      assert.equal(res.statusCode,400);
+      done();
+    });
+  };
+
+// Make a request to a random path
+api['A random path should respond to GET with 404'] = function(done){
+  helpers.makeGetRequest('/this/path/shouldnt/exist',function(res){
+    assert.equal(res.statusCode,404);
+    done();
+  });
+};
+
+// Export the tests to the runner
+module.exports = api;
